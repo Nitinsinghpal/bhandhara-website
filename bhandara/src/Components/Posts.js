cadc25255783ce7d94d9c43fbf6b8730c0054f3e
Nitin <nitinsingh919293@gmail.com>
2024-03-18 08:12:00 +0530

    SideNavAnimation

--- bhandara/src/Components/Posts.js
+++ bhandara/src/Components/Posts.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import './Posts.css'
-import PostsCard from './PostsCard'
-function Posts({data}) {
-  console.log(data)
+import React from "react";
+import "./Posts.css";
+import PostsCard from "./PostsCard";
+function Posts({ data }) {
+  console.log(data);
   function posts() {
-
     let cards = [];
-    data.map((d) =>{
-     cards.push(<PostsCard cardInfo={d}/>)
-    })
-    console.log(cards)
+    data.map((d) => {
+      cards.push(<PostsCard cardInfo={d} />);
+    });
+    console.log(cards);
     return cards;
   }
   return (
-    <div className='posts-main'>
+    <>
+      {data.length !== 0 ? (
+        <div className="posts-main">
+          <PostsCard cardInfo={data} />
+        </div>
+      ) : (
+        <div className="noPostAvailable-main">Post will display here</div>
+      )}
 
-    <PostsCard cardInfo={data}/>
-        {/* <PostsCard/>
+      {/* <PostsCard/>
         <PostsCard/>
         <PostsCard/>
         <PostsCard/>
         <PostsCard/> */}
-    
-    </div>
-  )
+    </>
+  );
 }
 
-export default Posts
+export default Posts;
